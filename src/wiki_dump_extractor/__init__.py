72a565685fa9983a6c3eafdda0acf2b5d02c024c
Zulko <valentin.zulkower@gmail.com>
2025-03-07 19:41:43 -0500

    added download_file

--- src/wiki_dump_extractor/__init__.py
+++ src/wiki_dump_extractor/__init__.py
@@ -3,5 +3,7 @@
 __version__ = "0.1.0"
 
 from .wiki_dump_extractor import WikiDumpExtractor
+from .wiki_sql_extractor import WikiSqlExtractor
+from .download_utils import download_file
 
-__all__ = ["WikiDumpExtractor"]
+__all__ = ["WikiDumpExtractor", "WikiSqlExtractor", "download_file"]
