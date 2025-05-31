0d3cc9e9270ea3a439a398d9983893b8f0442744
Zulko <valentin.zulkower+git@gmail.com>
2025-05-31 13:13:58 -0400

    adding docs

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "wiki_dump_extractor"
+copyright = "2025, Zulko"
+author = "Zulko"
+
+version = "0.1"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "en"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "shibuya"
+html_static_path = ["_static"]
---
+++ docs/source/ref.rst
@@ -0,0 +1,8 @@
+Wiki_dump_extractor reference
+-----------------------------
+
+Extractors
+~~~~~~~~~~
+
+.. automodule:: wiki_dump_extractor.wiki_dump_extractor
+  :members:
